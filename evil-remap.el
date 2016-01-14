fa7274574810a43c2f9aa5bdb67063108e7b86b5
Ben Moon <guiltydolphin@gmail.com>
2016-01-14 16:09:44 +0000

    Update header

--- evil-remap.el
+++ evil-remap.el
@@ -1,4 +1,5 @@
-;; Copyright (C) 2015 Ben Moon
+;;; evil-remap.el --- additional key-binding options for evil
+;; Copyright (C) 2016 Ben Moon
 ;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -13,6 +14,8 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+;;;
 ;;; Provide additional means of remapping keys in evil-mode
 ;;; akin to those found in Vim.
 ;;;
